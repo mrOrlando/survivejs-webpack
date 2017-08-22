051993256416e5ee04b13ade5de5f21253a06ac3
mrOrlando <juriy88@mail.com>
2017-08-22 14:18:37 +0300

    Composing config. Add devserver and linting JS.

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+require('dotenv').config(); // load config .env
+
+const parts = require('./webpack.parts');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+};
+
+const commonConfig = merge([
+  {
+    entry: {
+      app: PATHS.app,
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack demo',
+      }),
+      new webpack.LoaderOptionsPlugin({
+        options: {
+          eslint: {
+            // Fail only on errors
+            failOnWarning: false,
+            failOnError: true,
+
+            // Toggle autofix
+            fix: true,
+
+            // Output to Jenkins compatible XML
+            outputReport: {
+              filePath: 'checkstyle.xml',
+              formatter: require('eslint/lib/formatters/checkstyle'),
+            },
+          }
+        }
+      }),
+    ],
+  },
+  parts.lintJavaScript({
+    include: PATHS.app,
+    options: {
+      emitWarnings: true,
+    },
+  }),
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+]);
+
+module.exports = (env) => {
+  if (env === 'production') {
+    return merge(commonConfig, productionConfig);
+  }
+
+  return merge(commonConfig, developmentConfig);
+};
