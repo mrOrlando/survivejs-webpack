b571669c443894494fa297cbdfbf99cbd504e6a7
mrOrlando <juriy88@mail.com>
2017-08-24 13:33:34 +0300

    Add Autoprefixing.

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,9 @@ const commonConfig = merge([
 ]);
 
 const productionConfig = merge([
-  parts.extractCSS({ use: 'css-loader' }),
+  parts.extractCSS({
+    use: ['css-loader', parts.autoprefix()],
+  }),
 ]);
 
 const developmentConfig = merge([
--- webpack.parts.js
+++ webpack.parts.js
@@ -82,3 +82,12 @@ exports.extractCSS = ({ include, exclude, use }) => {
     plugins: [ plugin ],
   };
 };
+
+exports.autoprefix = () => ({
+  loader: 'postcss-loader',
+  options: {
+    plugins: () => ([
+      require('autoprefixer')(),
+    ]),
+  },
+});
